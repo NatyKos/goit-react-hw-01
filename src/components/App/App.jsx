7268ba4343d19b8c6c590b64a5f975985f310f82
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-03-31 18:42:30 +0100

    3.done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,9 @@
 import Profile from "../Profile/Profile";
 import FriendList from "../FriendList/FriendList";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
 import userData from "../../userData.json";
 import friends from "../../friends.json"
+import transactions from "../../transactions.json"
 
 export default function App() {
   return (
@@ -10,6 +12,7 @@ export default function App() {
         profile = {userData}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
